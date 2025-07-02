d74ccc43336ed23083450f6c1e0e9fd8e437006e
SamuelASilva <samuelavelinodasilvaaa@gmail.com>
2025-07-02 20:56:47 -0300

    Update backend.tf

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-iuri"
+    bucket = "terraform-state-aws-academy-iuri2"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
