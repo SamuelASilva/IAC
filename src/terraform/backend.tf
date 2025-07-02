93015f175404346777fa09d8e2f912a9fe9702a4
SamuelASilva <samuelavelinodasilvaaa@gmail.com>
2025-07-02 20:27:19 -0300

    Update test_workflow.yaml

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-20251-${var.student_id}"
+    bucket = "terraform-state-aws-academy-20251"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
